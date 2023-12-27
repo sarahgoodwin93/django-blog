81b056302854f9d7f2bab1a95d17f7e5e24dcb51
sarahgoodwin93 <>
2023-12-27 12:10:03 +0000

    add a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -92,6 +92,12 @@ DATABASES = {
 }
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
