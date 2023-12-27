846e9569d38e2814ab14a6852171c46580a1f365
sarahgoodwin93 <>
2023-12-27 13:56:11 +0000

    generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!em1xt!ez#ru_m_l1etlf^q^qayl8+x^(w&q49o1f-bi5q!-p='
+SECRET_KEY = 'django-insecure-!em1xt!ez#ru_m_l1etlf^q^qayl8+x^(w&q49o1f-bi5q!-p='  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-sarahgoodwin93-django-bl-zasivbagxh.us2.codeanyapp.com', '.herokuapp.com']
+    '8000-sarahgoodwin93-django-bl-zasivbagxh.us2.codeanyapp.com', '.herokuapp.com']  # noqa
 
 
 # Application definition
@@ -103,16 +103,16 @@ CSRF_TRUSTED_ORIGINS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
     },
 ]
 
