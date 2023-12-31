92678026cddd4b6045b742d17f5a5d1fc020958c
sarahgoodwin93 <>
2023-12-31 08:27:15 +0000

    update all files

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
-
 # Create your views here.
+
+
 class PostList(generic.ListView):
-    model = Post
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
@@ -24,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
