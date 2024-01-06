f60ff7cdbd090b743f996421495ef8991297bb3d
sarahgoodwin93 <>
2024-01-06 07:14:12 +0000

    complete form for about me

--- about/forms.py
+++ about/forms.py
@@ -2,7 +2,7 @@ from .models import CollaborateRequest
 from django import forms
 
 
-class CollabortForm(forms.ModelForm):
+class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
         fields = ('name', 'email', 'message')
